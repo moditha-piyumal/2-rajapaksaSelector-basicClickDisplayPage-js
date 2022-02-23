3b011a86979213c136c2efac583300e021ca17cd
Moditha Piyumal Wijeratne <100229199+moditha-piyumal@users.noreply.github.com>
2022-02-23 18:25:50 +0530

    Copied from my folder
    These are the files I have created in a separate folder. I entered them into the github repository folder.

---
+++ raja.js
@@ -0,0 +1,89 @@
+
+
+document.getElementById('viewerText').innerHTML="Ministerial Portfolio goes Here!";
+
+// I started doing this on my own attempting to make the small funny crud app without referring to any of my notes of any tutorials. I realised that I have forgotten some basic syntax. So, I have to make a small printed note on my own to remember certain basic stuff like... after innerHTML if I should use (brackets ) or = signs. 
+
+// However, my theory was correct. I knew what should be done to get the result. I also remembered how to make the css grid look like this, however, my grid did not work with uneven grid areas. So, I must make a sample grid again to see what I need to make a successful css grid with minimum code.
+
+let mahinda = document.getElementById('b1');
+
+mahinda.addEventListener('click', mahindaclick);
+
+function mahindaclick() {
+   document.getElementById('viewerText').innerHTML="God King";
+}
+
+
+let basil = document.getElementById('b2');
+
+basil.addEventListener('click', basilclick);
+
+function basilclick() {
+   document.getElementById('viewerText').innerHTML="Minister of Crime";
+}
+
+
+let gotabaya = document.getElementById('b3');
+
+gotabaya.addEventListener('click', gotabayaclick);
+
+function gotabayaclick() {
+   document.getElementById('viewerText').innerHTML="Minister of Defence";
+}
+
+
+let rohitha = document.getElementById('b4');
+
+rohitha.addEventListener('click', rohithaclick);
+
+function rohithaclick() {
+   document.getElementById('viewerText').innerHTML="Minister of Science";
+}
+
+
+let yoshitha = document.getElementById('b5');
+
+yoshitha.addEventListener('click', yoshithaclick);
+
+function yoshithaclick() {
+   document.getElementById('viewerText').innerHTML="Minister of Ships";
+}
+
+
+let namal = document.getElementById('b6');
+
+namal.addEventListener('click', namalclick);
+
+function namalclick() {
+   document.getElementById('viewerText').innerHTML="President";
+}
+
+
+let shashendra = document.getElementById('b7');
+
+shashendra.addEventListener('click', shashendraclick);
+
+function shashendraclick() {
+   document.getElementById('viewerText').innerHTML="Minister of Agriculture";
+}
+
+
+let tatyana = document.getElementById('b8');
+
+tatyana.addEventListener('click', tatyanaclick);
+
+function tatyanaclick() {
+   document.getElementById('viewerText').innerHTML="Minister of Women";
+}
+
+
+let nirwan = document.getElementById('b9');
+
+nirwan.addEventListener('click', nirwanclick);
+
+function nirwanclick() {
+   document.getElementById('viewerText').innerHTML="Minister of Boob Milk";
+}
+
+
